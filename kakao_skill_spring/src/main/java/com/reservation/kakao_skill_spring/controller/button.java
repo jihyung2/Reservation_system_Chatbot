fa992c81b1c4863ef298ea71ba217def64a2673d
jihyung2 <wlgud1116@naver.com>
2023-09-01 00:45:20 +0900

    asd

--- kakao_skill_spring/src/main/java/com/reservation/kakao_skill_spring/controller/button.java
+++ kakao_skill_spring/src/main/java/com/reservation/kakao_skill_spring/controller/button.java
@@ -1,26 +1,55 @@
 package com.reservation.kakao_skill_spring.controller;
 
-import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class button {
 
-    @RequestMapping(value = "/keyboard", method = RequestMethod.POST)
-    public String keyboard() {
-        System.out.println("/keyboard");
+    @Value("${kakao.api.key}")
+    private String kakaoApiKey;
 
-        JSONObject Btn1 = new JSONObject();
-        ArrayList<String> btns = new ArrayList<>();
-        btns.add("신청");
-        btns.add("나의 신청 내역 (고객센터)");
+    @PostMapping("/send-message")
+    public ResponseEntity<String> sendMessage(@RequestBody String userId) {
+        String sendMessageUrl = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
 
-        Btn1.put("type", "buttons");
-        Btn1.put("buttons", btns);
+        String messageJson = generateMessageJson(userId);
 
-        return Btn1.toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + kakaoApiKey);
+
+        HttpEntity<String> request = new HttpEntity<>(messageJson, headers);
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<String> response = restTemplate.postForEntity(sendMessageUrl, request, String.class);
+        return response;
+    }
+
+    private String generateMessageJson(String userId) {
+        // 메시지 JSON 생성 및 버튼 정보 설정
+        String messageJson = "{"
+                + "\"object_type\": \"text\","
+                + "\"text\": \"버튼을 눌러주세요!\","
+                + "\"link\": {"
+                + "\"web_url\": \"https://www.example.com\""
+                + "},"
+                + "\"buttons\": ["
+                + "{"
+                + "\"label\": \"버튼 1\","
+                + "\"action\": \"webLink\","
+                + "\"webLinkUrl\": \"https://www.example.com/button1\""
+                + "},"
+                + "{"
+                + "\"label\": \"버튼 2\","
+                + "\"action\": \"webLink\","
+                + "\"webLinkUrl\": \"https://www.example.com/button2\""
+                + "}"
+                + "]"
+                + "}";
+
+        return messageJson;
     }
 }
